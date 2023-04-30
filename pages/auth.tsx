d306833dcbaa13727f753e62e13f663c12a253bc
Tobias <113835194+optobimus@users.noreply.github.com>
2023-04-30 10:20:03 +0200

    Add Register API route

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import axios from "axios"
 
 const Auth = () => {
     const [email, setEmail] = useState("");
@@ -10,6 +11,18 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === "login" ? "register" : "login")
+    }, []);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post("/api/register", {
+                email,
+                name,
+                password
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }, []);
 
     return (
