cf0cefa33eabcbaaf8517d279b6299e752328e61
Tobias <113835194+optobimus@users.noreply.github.com>
2023-04-30 17:31:53 +0200

    Add Navigation bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -26,9 +27,7 @@ export default function Home() {
 
   return (
     <>
-      <h1>Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+      <Navbar />
     </>
   )
 }
